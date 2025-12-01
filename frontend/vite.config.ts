ab375906e4fc69140eda0fc3f8e44c5b4c0634ba
Lumate <sanchous557@mail.ru>
2025-12-01 19:24:56 +0300

    update vite.config
    - add auto-imports config for vue, pinia, router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
       resolvers: [ElementPlusResolver()],
       dts: true,
     }),
